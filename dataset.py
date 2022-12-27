5c5ff2a787df7c25b5c18bc357ad5531b8b6562c
zhShen-oc <18817658581@163.com>
2022-12-27 11:53:31 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,93 @@
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torchvision.transforms as transforms
+import glob
+import os
+import pytorch_lightning as pl
+import random
+import torch
+import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+class ImageDataset(Dataset):
+    def __init__(self, data_dir, istrain=True):
+        if istrain:
+            self.uw_image_path = os.path.join(data_dir, "train")
+        else:
+            self.uw_image_path = os.path.join(data_dir, "test")
+        self.gt_img_path = os.path.join(data_dir, "gt")
+        self.image_names = os.listdir(self.uw_image_path)
+        self.transform = A.Compose(
+            transforms=[
+                A.Resize(448, 608),
+                A.HorizontalFlip(p=0.5),
+                A.RandomCrop(256, 256, p=1),
+                ToTensorV2(),
+            ],
+            additional_targets={"image0": "image"},
+        )
+        if not istrain:
+            self.transform = A.Compose(
+                transforms=[A.Resize(448, 608), ToTensorV2()],
+                additional_targets={"image0": "image"},
+            )
+
+    def __getitem__(self, index):
+        image_name = self.image_names[index]
+        uw_image = np.array(Image.open(os.path.join(self.uw_image_path, image_name)))
+        gt_image = np.array(Image.open(os.path.join(self.gt_img_path, image_name)))
+
+        transformed = self.transform(image=uw_image, image0=gt_image)
+        underwater_image = transformed["image"]
+        gt_image = transformed["image0"]
+        underwater_image = underwater_image.float() / 255
+        gt_image = gt_image.float() / 255
+        return {
+            "underwater_image": underwater_image,
+            "gt_image": gt_image,
+            "image_name": image_name,
+        }
+
+    def __len__(self):
+        return len(self.image_names)
+
+
+class MyDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_set,
+        batch_size,
+        num_workers,
+    ):
+        super().__init__()
+        self.dataset = data_set
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage=None):
+        datadir = ""
+        if self.dataset == "nyu":
+            datadir = "/mnt/epnfs/zhshen/DE_code_0904/nyu_DATA"
+        elif self.dataset == "uieb":
+            datadir = "/mnt/epnfs/zhshen/DE_code_0904/UIEB"
+        self.train_data = ImageDataset(os.path.join(datadir, "Train"))
+        self.val_data = ImageDataset(os.path.join(datadir, "Test"), istrain=False)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_data,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.val_data, batch_size=self.batch_size, num_workers=self.num_workers
+        )
---
+++ train.py
@@ -0,0 +1,91 @@
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.utils import save_image
+# from torchmetrics.functional import structural_similarity_index_measure as ssim
+# from torchmetrics.functional import peak_signal_noise_ratio as psnr
+from model import *
+from dataset import *
+from CR import *
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+
+save_path = './results/'
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1: 
+        m.weight.data.normal_(0.0, 0.001)
+    if classname.find('Linear') != -1: 
+        m.weight.data.normal_(0.0, 0.001)
+
+class MyModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = MainModel()
+        self.model.apply(weights_init)
+        self.l1_loss = nn.L1Loss()
+        self.CR_loss = ContrastLoss()
+        self.save_path = save_path
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        uw_image, gt_image= batch['underwater_image'],batch['gt_image']
+        trans_map, atm_map, clear_image = self(uw_image)
+        clear_image = torch.clamp(clear_image, 0, 1)
+        l1_loss = self.l1_loss(clear_image,gt_image)
+        cr_loss = self.CR_loss(clear_image,gt_image,uw_image)
+        loss = cr_loss
+        self.log("loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        uw_image, gt_image = batch['underwater_image'],batch['gt_image']
+        image_name = batch['image_name']
+        _trans, _atm, _GT = self(uw_image)
+        _GT = torch.clamp(_GT, 0, 1)
+        for i in range(len(image_name)):
+            # save_image(_trans[i], os.path.join(self.save_path, 'trans_'+image_name[i]))
+            # save_image(_atm[i], os.path.join(self.save_path, 'atm_'+image_name[i]))
+            save_image(_GT[i], os.path.join(self.save_path, image_name[i]))
+
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=None):
+        uw_image, gt_image = batch['underwater_image'],batch['gt_image']
+        image_name = batch['image_name']
+        _trans, _atm, _GT = self(uw_image)
+        _GT = torch.clamp(_GT, 0, 1)
+        for i in range(len(image_name)):
+            # save_image(_trans[i], os.path.join(self.save_path, 'trans_'+image_name[i]))
+            # save_image(_atm[i], os.path.join(self.save_path, 'atm_'+image_name[i]))
+            save_image(_GT[i], os.path.join(self.save_path, image_name[i]))
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4, betas=(0.99, 0.999), eps=1e-08, weight_decay=1e-2)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, factor=0.5, verbose=True)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "loss"}
+
+
+if __name__ == '__main__':
+    pl.seed_everything(1234)
+    dm = MyDataModule(data_set='uieb',
+                            batch_size = 32,
+                            num_workers = 64)
+    n_epochs = 150
+    model = MyModel()
+    # model.load_state_dict(torch.load(os.path.join('/mnt/epnfs/zhshen/DE_code_0904/results/mynet_uieb', 'model.pt')))
+    trainer = pl.Trainer(
+        accelerator="auto",
+        devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
+        max_epochs=n_epochs,
+        check_val_every_n_epoch=10,
+    )
+    trainer.fit(model, datamodule = dm)
+    # predictions = trainer.predict(model, datamodule = dm)
+    torch.save(model.state_dict(), os.path.join(save_path,'model.pt'))
+
+    
